e1e208264688ec09e6683facae243c55afa99135
marqs <mhiienka@niksula.hut.fi>
2020-03-09 23:58:29 +0200

    add RTL and SW code

---
+++ de10-nano/DE10-Nano-vd_isl.v
@@ -0,0 +1,353 @@
+//`define ENABLE_HPS
+
+module DE10_Nano_vd_isl (
+
+      ///////// ADC /////////
+      output             ADC_CONVST,
+      output             ADC_SCK,
+      output             ADC_SDI,
+      input              ADC_SDO,
+
+      ///////// ARDUINO /////////
+      inout       [15:0] ARDUINO_IO,
+      inout              ARDUINO_RESET_N,
+
+      ///////// FPGA /////////
+      input              FPGA_CLK1_50,
+      input              FPGA_CLK2_50,
+      input              FPGA_CLK3_50,
+
+      ///////// GPIO /////////
+      inout       [35:0] GPIO_0,
+      inout       [35:0] GPIO_1,
+
+      ///////// HDMI /////////
+      inout              HDMI_I2C_SCL,
+      inout              HDMI_I2C_SDA,
+      inout              HDMI_I2S,
+      inout              HDMI_LRCLK,
+      inout              HDMI_MCLK,
+      inout              HDMI_SCLK,
+      output             HDMI_TX_CLK,
+      output      [23:0] HDMI_TX_D,
+      output             HDMI_TX_DE,
+      output             HDMI_TX_HS,
+      input              HDMI_TX_INT,
+      output             HDMI_TX_VS,
+
+`ifdef ENABLE_HPS
+      ///////// HPS /////////
+      inout              HPS_CONV_USB_N,
+      output      [14:0] HPS_DDR3_ADDR,
+      output      [2:0]  HPS_DDR3_BA,
+      output             HPS_DDR3_CAS_N,
+      output             HPS_DDR3_CKE,
+      output             HPS_DDR3_CK_N,
+      output             HPS_DDR3_CK_P,
+      output             HPS_DDR3_CS_N,
+      output      [3:0]  HPS_DDR3_DM,
+      inout       [31:0] HPS_DDR3_DQ,
+      inout       [3:0]  HPS_DDR3_DQS_N,
+      inout       [3:0]  HPS_DDR3_DQS_P,
+      output             HPS_DDR3_ODT,
+      output             HPS_DDR3_RAS_N,
+      output             HPS_DDR3_RESET_N,
+      input              HPS_DDR3_RZQ,
+      output             HPS_DDR3_WE_N,
+      output             HPS_ENET_GTX_CLK,
+      inout              HPS_ENET_INT_N,
+      output             HPS_ENET_MDC,
+      inout              HPS_ENET_MDIO,
+      input              HPS_ENET_RX_CLK,
+      input       [3:0]  HPS_ENET_RX_DATA,
+      input              HPS_ENET_RX_DV,
+      output      [3:0]  HPS_ENET_TX_DATA,
+      output             HPS_ENET_TX_EN,
+      inout              HPS_GSENSOR_INT,
+      inout              HPS_I2C0_SCLK,
+      inout              HPS_I2C0_SDAT,
+      inout              HPS_I2C1_SCLK,
+      inout              HPS_I2C1_SDAT,
+      inout              HPS_KEY,
+      inout              HPS_LED,
+      inout              HPS_LTC_GPIO,
+      output             HPS_SD_CLK,
+      inout              HPS_SD_CMD,
+      inout       [3:0]  HPS_SD_DATA,
+      output             HPS_SPIM_CLK,
+      input              HPS_SPIM_MISO,
+      output             HPS_SPIM_MOSI,
+      inout              HPS_SPIM_SS,
+      input              HPS_UART_RX,
+      output             HPS_UART_TX,
+      input              HPS_USB_CLKOUT,
+      inout       [7:0]  HPS_USB_DATA,
+      input              HPS_USB_DIR,
+      input              HPS_USB_NXT,
+      output             HPS_USB_STP,
+`endif /*ENABLE_HPS*/
+
+      ///////// KEY /////////
+      input       [1:0]  KEY,
+
+      ///////// LED /////////
+      output      [7:0]  LED,
+
+      ///////// SW /////////
+      input       [3:0]  SW
+);
+
+
+//=======================================================
+//  REG/WIRE declarations
+//=======================================================
+
+wire clk27, PCLK_sc, pclk_out;
+wire SI_PCLK_i = GPIO_0[0];
+wire ISL_PCLK_i = GPIO_0[2];
+wire sys_reset_n = 1'b1;
+wire [7:0] ISL_R_i = {GPIO_0[22], GPIO_0[23], GPIO_0[24], GPIO_0[25], GPIO_0[26], GPIO_0[27], GPIO_0[28], GPIO_0[29]};
+wire [7:0] ISL_G_i = {GPIO_0[14], GPIO_0[15], GPIO_0[16], GPIO_0[17], GPIO_0[18], GPIO_0[19], GPIO_0[20], GPIO_0[21]};
+wire [7:0] ISL_B_i = {GPIO_0[6], GPIO_0[7], GPIO_0[8], GPIO_0[9], GPIO_0[10], GPIO_0[11], GPIO_0[12], GPIO_0[13]};
+wire ISL_HS_i = GPIO_0[30];
+wire ISL_HSYNC_i = GPIO_0[31];
+wire ISL_VSYNC_i = GPIO_0[32];
+wire ISL_FID_i = GPIO_0[35];
+wire ISL_INT_N_i = GPIO_0[33];
+wire IR_RX_i = GPIO_0[34];
+wire pclk_capture = ISL_PCLK_i;
+
+wire [15:0] sys_ctrl;
+wire sys_extra = sys_ctrl[0];
+wire isl_reset_n = sys_ctrl[1];
+wire hdmirx_reset_n = sys_ctrl[2];
+wire emif_hwreset_n = sys_ctrl[3];
+wire emif_swreset_n = sys_ctrl[4];
+wire capture_sel = sys_ctrl[5];
+wire isl_vs_pol = sys_ctrl[6];
+wire isl_vs_type = sys_ctrl[7];
+wire audmux_sel = sys_ctrl[8];
+wire testpattern_enable = sys_ctrl[9];
+
+//reg [1:0] clk_osc_div = 2'h0;
+//wire [31:0] h_in_config, h_in_config2, v_in_config, h_out_config, h_out_config2, v_out_config, v_out_config2;
+/*wire SCL = GPIO_0[5] & HDMI_I2C_SCL;
+wire SDA = GPIO_0[4] & HDMI_I2C_SDA;*/
+/*assign GPIO_0[5] = scl_oe ? 1'b0 : 1'bz;
+assign GPIO_0[4] = sda_oe ? 1'b0 : 1'bz;*/
+/*assign HDMI_I2C_SCL = scl_oe ? 1'b0 : 1'bz;
+assign HDMI_I2C_SDA = sda_oe ? 1'b0 : 1'bz;*/
+
+reg ir_rx_sync1_reg, ir_rx_sync2_reg;
+reg [1:0] btn_sync1_reg, btn_sync2_reg;
+
+wire [15:0] ir_code;
+wire [7:0] ir_code_cnt;
+
+wire scl_oe, sda_oe;
+wire pll_lock;
+wire nios_reset_req;
+
+wire [31:0] sys_status = {6'h0, btn_sync2_reg, ir_code_cnt, ir_code};
+
+wire [31:0] h_in_config, h_in_config2, v_in_config, h_out_config, h_out_config2, v_out_config, v_out_config2, xy_out_config;
+
+reg [23:0] resync_led_ctr;
+reg resync_strobe_sync1_reg, resync_strobe_sync2_reg, resync_strobe_prev;
+wire resync_strobe_i;
+wire resync_strobe = resync_strobe_sync2_reg;
+
+assign LED = {pll_lock, 5'h0, (ir_code == 0), (resync_led_ctr != 0)};
+
+
+
+// ISL51002 RGB digitizer
+reg [7:0] ISL_R, ISL_G, ISL_B;
+reg ISL_HS;
+reg ISL_DE;
+reg ISL_FID;
+reg ISL_HSYNC_sync1_reg, ISL_HSYNC_sync2_reg;
+reg ISL_VSYNC_sync1_reg, ISL_VSYNC_sync2_reg;
+always @(posedge ISL_PCLK_i) begin
+    ISL_R <= ISL_R_i;
+    ISL_G <= ISL_G_i;
+    ISL_B <= ISL_B_i;
+    ISL_HS <= ISL_HS_i;
+    ISL_FID <= ISL_FID_i;
+
+    // sync to pclk
+    ISL_HSYNC_sync1_reg <= ISL_HSYNC_i;
+    ISL_HSYNC_sync2_reg <= ISL_HSYNC_sync1_reg;
+    ISL_VSYNC_sync1_reg <= ISL_VSYNC_i;
+    ISL_VSYNC_sync2_reg <= ISL_VSYNC_sync1_reg;
+end
+
+wire [7:0] ISL_R_post, ISL_G_post, ISL_B_post;
+wire ISL_HSYNC_post, ISL_VSYNC_post, ISL_DE_post, ISL_FID_post;
+wire ISL_fe_interlace, ISL_fe_frame_change;
+wire [19:0] ISL_fe_pcnt_frame;
+wire [10:0] ISL_fe_vtotal, ISL_fe_xpos, ISL_fe_ypos;
+isl51002_frontend u_isl_frontend ( 
+    .PCLK_i(ISL_PCLK_i),
+    .CLK_MEAS_i(clk27),
+    .reset_n(sys_reset_n),
+    .R_i(ISL_R),
+    .G_i(ISL_G),
+    .B_i(ISL_B),
+    .HS_i(ISL_HS),
+    .HSYNC_i(ISL_HSYNC_sync2_reg),
+    .VSYNC_i(ISL_VSYNC_sync2_reg),
+    .DE_i(ISL_DE),
+    .FID_i(ISL_FID),
+    .vs_type(isl_vs_type),
+    .vs_polarity(isl_vs_pol),
+    .h_in_config(h_in_config),
+    .h_in_config2(h_in_config2),
+    .v_in_config(v_in_config),
+    .R_o(ISL_R_post),
+    .G_o(ISL_G_post),
+    .B_o(ISL_B_post),
+    .HSYNC_o(ISL_HSYNC_post),
+    .VSYNC_o(ISL_VSYNC_post),
+    .DE_o(ISL_DE_post),
+    .FID_o(ISL_FID_post),
+    .interlace_flag(ISL_fe_interlace),
+    .xpos(ISL_fe_xpos),
+    .ypos(ISL_fe_ypos),
+    .vtotal(ISL_fe_vtotal),
+    .frame_change(ISL_fe_frame_change),
+    .pcnt_frame(ISL_fe_pcnt_frame)
+);
+
+// output clock assignment
+assign pclk_out = PCLK_sc;
+assign HDMI_TX_CLK = pclk_out;
+
+// output data assignment
+wire [7:0] R_sc, G_sc, B_sc;
+wire HSYNC_sc, VSYNC_sc, DE_sc;
+always @(posedge pclk_out) begin
+    HDMI_TX_D[23:16] <= R_sc;
+    HDMI_TX_D[15:8] <= G_sc;
+    HDMI_TX_D[7:0] <= B_sc;
+    HDMI_TX_HS <= HSYNC_sc;
+    HDMI_TX_VS <= VSYNC_sc;
+    HDMI_TX_DE <= DE_sc;
+end
+
+//audio
+assign HDMI_SCLK = GPIO_1[2];
+assign HDMI_LRCLK = GPIO_1[6];
+assign HDMI_I2S = GPIO_1[4];
+//assign HDMI_SPDIF = SPDIF_EXT_i;
+
+always @(posedge clk27) begin
+    if (~resync_strobe_prev & resync_strobe) begin
+        resync_led_ctr <= {24{1'b1}};
+    end else if (resync_led_ctr > 0) begin
+        resync_led_ctr <= resync_led_ctr - 1'b1;
+    end
+
+    resync_strobe_sync1_reg <= resync_strobe_i;
+    resync_strobe_sync2_reg <= resync_strobe_sync1_reg;
+    resync_strobe_prev <= resync_strobe_sync2_reg;
+end
+
+// Insert synchronizers to async inputs (synchronize to CPU clock)
+always @(posedge clk27 or negedge sys_reset_n) begin
+    if (!sys_reset_n) begin
+        btn_sync1_reg <= 2'b11;
+        btn_sync2_reg <= 2'b11;
+        ir_rx_sync1_reg <= 1'b1;
+        ir_rx_sync2_reg <= 1'b1;
+    end else begin
+        btn_sync1_reg <= KEY;
+        btn_sync2_reg <= btn_sync1_reg;
+        ir_rx_sync1_reg <= IR_RX_i;
+        ir_rx_sync2_reg <= ir_rx_sync1_reg;
+    end
+end
+
+pll pll_sys (
+    .refclk(FPGA_CLK1_50),
+    .rst(1'b0),
+    .outclk_0(clk27),
+    .locked(pll_lock)
+);
+
+sys u0 (
+    .clk_clk                 (clk27),                 //              clk.clk
+    .reset_reset_n           (sys_reset_n),            //            reset.reset_n
+    /*.i2c_0_i2c_serial_sda_in (HDMI_I2C_SDA), // i2c_0_i2c_serial.sda_in
+    .i2c_0_i2c_serial_scl_in (HDMI_I2C_SCL), //                 .scl_in
+    .i2c_0_i2c_serial_sda_oe (sda_oe), //                 .sda_oe
+    .i2c_0_i2c_serial_scl_oe (scl_oe), //                 .scl_oe*/
+    .i2c_opencores_0_export_scl_pad_io      (GPIO_0[5]),
+    .i2c_opencores_0_export_sda_pad_io      (GPIO_0[4]),
+    .i2c_opencores_0_export_spi_miso_pad_i  (1'b0),
+    .i2c_opencores_1_export_scl_pad_io      (HDMI_I2C_SCL),
+    .i2c_opencores_1_export_sda_pad_io      (HDMI_I2C_SDA),
+    .i2c_opencores_1_export_spi_miso_pad_i  (1'b0),
+    .pio_0_sys_ctrl_out_export              (sys_ctrl),
+    .pio_1_controls_in_export               (sys_status),
+    .sc_config_0_sc_if_sc_status_i          ({20'h0, ISL_fe_interlace, ISL_fe_vtotal}),
+    .sc_config_0_sc_if_sc_status2_i         ({12'h0, ISL_fe_pcnt_frame}),
+    .sc_config_0_sc_if_lt_status_i          (32'h00000000),
+    .sc_config_0_sc_if_h_in_config_o        (h_in_config),
+    .sc_config_0_sc_if_h_in_config2_o       (h_in_config2),
+    .sc_config_0_sc_if_v_in_config_o        (v_in_config),
+    .sc_config_0_sc_if_misc_config_o        (),
+    .sc_config_0_sc_if_sl_config_o          (),
+    .sc_config_0_sc_if_sl_config2_o         (),
+    .sc_config_0_sc_if_h_out_config_o       (h_out_config),
+    .sc_config_0_sc_if_h_out_config2_o      (h_out_config2),
+    .sc_config_0_sc_if_v_out_config_o       (v_out_config),
+    .sc_config_0_sc_if_v_out_config2_o      (v_out_config2),
+    .sc_config_0_sc_if_xy_out_config_o      (xy_out_config)
+);
+
+scanconverter scanconverter_inst (
+    .PCLK_CAP_i(pclk_capture),
+    .PCLK_OUT_i(SI_PCLK_i),
+    .reset_n(sys_reset_n),  //TODO: sync to pclk_capture
+    .R_i(ISL_R_post),
+    .G_i(ISL_G_post),
+    .B_i(ISL_B_post),
+    .HSYNC_i(ISL_HSYNC_post),
+    .VSYNC_i(ISL_VSYNC_post),
+    .DE_i(ISL_DE_post),
+    .FID_i(ISL_FID_post),
+    .frame_change_i(ISL_fe_frame_change),
+    .xpos_i(ISL_fe_xpos),
+    .ypos_i(ISL_fe_ypos),
+    .h_out_config(h_out_config),
+    .h_out_config2(h_out_config2),
+    .v_out_config(v_out_config),
+    .v_out_config2(v_out_config2),
+    .xy_out_config(xy_out_config),
+    .misc_config(32'h0),
+    .sl_config(32'h0),
+    .sl_config2(32'h0),
+    .testpattern_enable(testpattern_enable),
+    .PCLK_o(PCLK_sc),
+    .R_o(R_sc),
+    .G_o(G_sc),
+    .B_o(B_sc),
+    .HSYNC_o(HSYNC_sc),
+    .VSYNC_o(VSYNC_sc),
+    .DE_o(DE_sc),
+    .xpos_o(),
+    .ypos_o(),
+    .resync_strobe(resync_strobe_i)
+);
+
+ir_rcv ir0 (
+    .clk27          (clk27),
+    .reset_n        (sys_reset_n),
+    .ir_rx          (ir_rx_sync2_reg),
+    .ir_code        (ir_code),
+    .ir_code_ack    (),
+    .ir_code_cnt    (ir_code_cnt)
+);
+
+endmodule
---
+++ software/sys_controller_bsp/system.h
@@ -0,0 +1,349 @@
+/*
+ * system.h - SOPC Builder system and BSP software package information
+ *
+ * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'sys'
+ * SOPC Builder design path: ../../de10-nano/sys.sopcinfo
+ *
+ * Generated: Sun Mar 08 22:44:31 EET 2020
+ */
+
+/*
+ * DO NOT MODIFY THIS FILE
+ *
+ * Changing this file will have subtle consequences
+ * which will almost certainly lead to a nonfunctioning
+ * system. If you do modify this file, be aware that your
+ * changes will be overwritten and lost when this file
+ * is generated again.
+ *
+ * DO NOT MODIFY THIS FILE
+ */
+
+/*
+ * License Agreement
+ *
+ * Copyright (c) 2008
+ * Altera Corporation, San Jose, California, USA.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This agreement shall be governed in all respects by the laws of the State
+ * of California and by the laws of the United States of America.
+ */
+
+#ifndef __SYSTEM_H_
+#define __SYSTEM_H_
+
+/* Include definitions from linker script generator */
+#include "linker.h"
+
+
+/*
+ * CPU configuration
+ *
+ */
+
+#define ALT_CPU_ARCHITECTURE "altera_nios2_gen2"
+#define ALT_CPU_BIG_ENDIAN 0
+#define ALT_CPU_BREAK_ADDR 0x00020820
+#define ALT_CPU_CPU_ARCH_NIOS2_R1
+#define ALT_CPU_CPU_FREQ 27000000u
+#define ALT_CPU_CPU_ID_SIZE 1
+#define ALT_CPU_CPU_ID_VALUE 0x00000000
+#define ALT_CPU_CPU_IMPLEMENTATION "tiny"
+#define ALT_CPU_DATA_ADDR_WIDTH 0x12
+#define ALT_CPU_DCACHE_LINE_SIZE 0
+#define ALT_CPU_DCACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_DCACHE_SIZE 0
+#define ALT_CPU_EXCEPTION_ADDR 0x00010020
+#define ALT_CPU_FLASH_ACCELERATOR_LINES 0
+#define ALT_CPU_FLASH_ACCELERATOR_LINE_SIZE 0
+#define ALT_CPU_FLUSHDA_SUPPORTED
+#define ALT_CPU_FREQ 27000000
+#define ALT_CPU_HARDWARE_DIVIDE_PRESENT 0
+#define ALT_CPU_HARDWARE_MULTIPLY_PRESENT 0
+#define ALT_CPU_HARDWARE_MULX_PRESENT 0
+#define ALT_CPU_HAS_DEBUG_CORE 1
+#define ALT_CPU_HAS_DEBUG_STUB
+#define ALT_CPU_HAS_ILLEGAL_INSTRUCTION_EXCEPTION
+#define ALT_CPU_HAS_JMPI_INSTRUCTION
+#define ALT_CPU_ICACHE_LINE_SIZE 0
+#define ALT_CPU_ICACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_ICACHE_SIZE 0
+#define ALT_CPU_INST_ADDR_WIDTH 0x12
+#define ALT_CPU_NAME "nios2_gen2_0"
+#define ALT_CPU_OCI_VERSION 1
+#define ALT_CPU_RESET_ADDR 0x00010000
+
+
+/*
+ * CPU configuration (with legacy prefix - don't use these anymore)
+ *
+ */
+
+#define NIOS2_BIG_ENDIAN 0
+#define NIOS2_BREAK_ADDR 0x00020820
+#define NIOS2_CPU_ARCH_NIOS2_R1
+#define NIOS2_CPU_FREQ 27000000u
+#define NIOS2_CPU_ID_SIZE 1
+#define NIOS2_CPU_ID_VALUE 0x00000000
+#define NIOS2_CPU_IMPLEMENTATION "tiny"
+#define NIOS2_DATA_ADDR_WIDTH 0x12
+#define NIOS2_DCACHE_LINE_SIZE 0
+#define NIOS2_DCACHE_LINE_SIZE_LOG2 0
+#define NIOS2_DCACHE_SIZE 0
+#define NIOS2_EXCEPTION_ADDR 0x00010020
+#define NIOS2_FLASH_ACCELERATOR_LINES 0
+#define NIOS2_FLASH_ACCELERATOR_LINE_SIZE 0
+#define NIOS2_FLUSHDA_SUPPORTED
+#define NIOS2_HARDWARE_DIVIDE_PRESENT 0
+#define NIOS2_HARDWARE_MULTIPLY_PRESENT 0
+#define NIOS2_HARDWARE_MULX_PRESENT 0
+#define NIOS2_HAS_DEBUG_CORE 1
+#define NIOS2_HAS_DEBUG_STUB
+#define NIOS2_HAS_ILLEGAL_INSTRUCTION_EXCEPTION
+#define NIOS2_HAS_JMPI_INSTRUCTION
+#define NIOS2_ICACHE_LINE_SIZE 0
+#define NIOS2_ICACHE_LINE_SIZE_LOG2 0
+#define NIOS2_ICACHE_SIZE 0
+#define NIOS2_INST_ADDR_WIDTH 0x12
+#define NIOS2_OCI_VERSION 1
+#define NIOS2_RESET_ADDR 0x00010000
+
+
+/*
+ * Define for each module class mastered by the CPU
+ *
+ */
+
+#define __ALTERA_AVALON_JTAG_UART
+#define __ALTERA_AVALON_ONCHIP_MEMORY2
+#define __ALTERA_AVALON_PIO
+#define __ALTERA_AVALON_SYSID_QSYS
+#define __ALTERA_NIOS2_GEN2
+#define __I2C_OPENCORES
+#define __SC_CONFIG
+
+
+/*
+ * System configuration
+ *
+ */
+
+#define ALT_DEVICE_FAMILY "Cyclone V"
+#define ALT_ENHANCED_INTERRUPT_API_PRESENT
+#define ALT_IRQ_BASE NULL
+#define ALT_LOG_PORT "/dev/null"
+#define ALT_LOG_PORT_BASE 0x0
+#define ALT_LOG_PORT_DEV null
+#define ALT_LOG_PORT_TYPE ""
+#define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
+#define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
+#define ALT_NUM_INTERRUPT_CONTROLLERS 1
+#define ALT_STDERR "/dev/jtag_uart_0"
+#define ALT_STDERR_BASE 0x210a8
+#define ALT_STDERR_DEV jtag_uart_0
+#define ALT_STDERR_IS_JTAG_UART
+#define ALT_STDERR_PRESENT
+#define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDIN "/dev/jtag_uart_0"
+#define ALT_STDIN_BASE 0x210a8
+#define ALT_STDIN_DEV jtag_uart_0
+#define ALT_STDIN_IS_JTAG_UART
+#define ALT_STDIN_PRESENT
+#define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDOUT "/dev/jtag_uart_0"
+#define ALT_STDOUT_BASE 0x210a8
+#define ALT_STDOUT_DEV jtag_uart_0
+#define ALT_STDOUT_IS_JTAG_UART
+#define ALT_STDOUT_PRESENT
+#define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
+#define ALT_SYSTEM_NAME "sys"
+
+
+/*
+ * hal configuration
+ *
+ */
+
+#define ALT_MAX_FD 8
+#define ALT_SYS_CLK none
+#define ALT_TIMESTAMP_CLK none
+
+
+/*
+ * i2c_opencores_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_i2c_opencores_0 i2c_opencores
+#define I2C_OPENCORES_0_BASE 0x21060
+#define I2C_OPENCORES_0_IRQ 0
+#define I2C_OPENCORES_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define I2C_OPENCORES_0_NAME "/dev/i2c_opencores_0"
+#define I2C_OPENCORES_0_SPAN 32
+#define I2C_OPENCORES_0_TYPE "i2c_opencores"
+
+
+/*
+ * i2c_opencores_1 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_i2c_opencores_1 i2c_opencores
+#define I2C_OPENCORES_1_BASE 0x21040
+#define I2C_OPENCORES_1_IRQ 2
+#define I2C_OPENCORES_1_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define I2C_OPENCORES_1_NAME "/dev/i2c_opencores_1"
+#define I2C_OPENCORES_1_SPAN 32
+#define I2C_OPENCORES_1_TYPE "i2c_opencores"
+
+
+/*
+ * jtag_uart_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_jtag_uart_0 altera_avalon_jtag_uart
+#define JTAG_UART_0_BASE 0x210a8
+#define JTAG_UART_0_IRQ 1
+#define JTAG_UART_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_UART_0_NAME "/dev/jtag_uart_0"
+#define JTAG_UART_0_READ_DEPTH 64
+#define JTAG_UART_0_READ_THRESHOLD 8
+#define JTAG_UART_0_SPAN 8
+#define JTAG_UART_0_TYPE "altera_avalon_jtag_uart"
+#define JTAG_UART_0_WRITE_DEPTH 64
+#define JTAG_UART_0_WRITE_THRESHOLD 8
+
+
+/*
+ * onchip_memory2_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_onchip_memory2_0 altera_avalon_onchip_memory2
+#define ONCHIP_MEMORY2_0_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define ONCHIP_MEMORY2_0_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define ONCHIP_MEMORY2_0_BASE 0x10000
+#define ONCHIP_MEMORY2_0_CONTENTS_INFO ""
+#define ONCHIP_MEMORY2_0_DUAL_PORT 0
+#define ONCHIP_MEMORY2_0_GUI_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_MEMORY2_0_INIT_CONTENTS_FILE "sys_onchip_memory2_0"
+#define ONCHIP_MEMORY2_0_INIT_MEM_CONTENT 1
+#define ONCHIP_MEMORY2_0_INSTANCE_ID "NONE"
+#define ONCHIP_MEMORY2_0_IRQ -1
+#define ONCHIP_MEMORY2_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ONCHIP_MEMORY2_0_NAME "/dev/onchip_memory2_0"
+#define ONCHIP_MEMORY2_0_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define ONCHIP_MEMORY2_0_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_MEMORY2_0_READ_DURING_WRITE_MODE "DONT_CARE"
+#define ONCHIP_MEMORY2_0_SINGLE_CLOCK_OP 1
+#define ONCHIP_MEMORY2_0_SIZE_MULTIPLE 1
+#define ONCHIP_MEMORY2_0_SIZE_VALUE 65536
+#define ONCHIP_MEMORY2_0_SPAN 65536
+#define ONCHIP_MEMORY2_0_TYPE "altera_avalon_onchip_memory2"
+#define ONCHIP_MEMORY2_0_WRITABLE 1
+
+
+/*
+ * pio_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pio_0 altera_avalon_pio
+#define PIO_0_BASE 0x21090
+#define PIO_0_BIT_CLEARING_EDGE_REGISTER 0
+#define PIO_0_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define PIO_0_CAPTURE 0
+#define PIO_0_DATA_WIDTH 16
+#define PIO_0_DO_TEST_BENCH_WIRING 0
+#define PIO_0_DRIVEN_SIM_VALUE 0
+#define PIO_0_EDGE_TYPE "NONE"
+#define PIO_0_FREQ 27000000
+#define PIO_0_HAS_IN 0
+#define PIO_0_HAS_OUT 1
+#define PIO_0_HAS_TRI 0
+#define PIO_0_IRQ -1
+#define PIO_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PIO_0_IRQ_TYPE "NONE"
+#define PIO_0_NAME "/dev/pio_0"
+#define PIO_0_RESET_VALUE 0
+#define PIO_0_SPAN 16
+#define PIO_0_TYPE "altera_avalon_pio"
+
+
+/*
+ * pio_1 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pio_1 altera_avalon_pio
+#define PIO_1_BASE 0x21080
+#define PIO_1_BIT_CLEARING_EDGE_REGISTER 0
+#define PIO_1_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define PIO_1_CAPTURE 0
+#define PIO_1_DATA_WIDTH 32
+#define PIO_1_DO_TEST_BENCH_WIRING 0
+#define PIO_1_DRIVEN_SIM_VALUE 0
+#define PIO_1_EDGE_TYPE "NONE"
+#define PIO_1_FREQ 27000000
+#define PIO_1_HAS_IN 1
+#define PIO_1_HAS_OUT 0
+#define PIO_1_HAS_TRI 0
+#define PIO_1_IRQ -1
+#define PIO_1_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PIO_1_IRQ_TYPE "NONE"
+#define PIO_1_NAME "/dev/pio_1"
+#define PIO_1_RESET_VALUE 0
+#define PIO_1_SPAN 16
+#define PIO_1_TYPE "altera_avalon_pio"
+
+
+/*
+ * sc_config_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sc_config_0 sc_config
+#define SC_CONFIG_0_BASE 0x21000
+#define SC_CONFIG_0_IRQ -1
+#define SC_CONFIG_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SC_CONFIG_0_NAME "/dev/sc_config_0"
+#define SC_CONFIG_0_SPAN 64
+#define SC_CONFIG_0_TYPE "sc_config"
+
+
+/*
+ * sysid_qsys_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sysid_qsys_0 altera_avalon_sysid_qsys
+#define SYSID_QSYS_0_BASE 0x210a0
+#define SYSID_QSYS_0_ID 291
+#define SYSID_QSYS_0_IRQ -1
+#define SYSID_QSYS_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SYSID_QSYS_0_NAME "/dev/sysid_qsys_0"
+#define SYSID_QSYS_0_SPAN 8
+#define SYSID_QSYS_0_TIMESTAMP 1583700121
+#define SYSID_QSYS_0_TYPE "altera_avalon_sysid_qsys"
+
+#endif /* __SYSTEM_H_ */
